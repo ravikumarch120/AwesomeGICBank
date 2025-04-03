49b6edbbc9a75feb2ce4226f1e2113115d182710
ravikumarch120 <ravikumarch.120@gmail.com>
2025-04-03 11:05:43 +0800

    code fix for unit test cases , commit

---
+++ AwesomeGICBankTest.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿namespace AwesomeGICBankTest.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            var expected = 5;
+            var actual = 2 + 3;
+            // Act
+            // (No action needed in this test)
+            // Assert
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
